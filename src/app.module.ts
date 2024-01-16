6e0c7e48cea1c82398e0ee41253a090dc3db2968
Sergey Uchaev <dialoggg1994@gmail.com>
2024-01-16 23:58:07 +0300

    Add db and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // тип базы данных
+      host: 'localhost', // хост, используйте 'postgres' если запускаете через Docker Compose
+      port: 5432, // порт, по умолчанию для PostgreSQL
+      username: 'ADMIN', // ваше имя пользователя в PostgreSQL
+      password: 'ROOT', // ваш пароль
+      database: 'DB', // имя вашей базы данных
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // путь к вашим сущностям (таблицам)
+      synchronize: true, // если true, TypeORM будет автоматически создавать таблицы на основе ваших сущностей при запуске приложения
+    }),
+    // другие модули...
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
