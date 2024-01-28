1e17fb555d33108cba72ce6d75597e6d03600d27
Sergey Uchaev <dialoggg1994@gmail.com>
2024-01-28 18:58:44 +0300

    Add JWT auth

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,13 +4,9 @@ import { Request } from 'express';
 
 @Controller('proxy/user')
 export class UserController {
-  // Здесь мы применяем JwtAuthGuard ко всем маршрутам в этом контроллере
   @UseGuards(JwtAuthGuard)
   @Get('info')
   getUserInfo(@Req() req: Request) {
-    // Здесь мы извлекаем пользователя из запроса и возвращаем его.
-    // Предполагается, что req.user установлен JwtStrategy после успешной аутентификации.
-    const user = req.user;
-    return user;
+    return req.user;
   }
 }
--- src/shared/types/types.d.ts
+++ src/shared/types/types.d.ts
@@ -1,9 +1,9 @@
-import { User } from '../../modules/user/entity/user.entity';
-
 declare global {
   namespace Express {
     interface Request {
-      user?: User; // Добавлено '?' для того, чтобы свойство было необязательным
+      user?: {
+        id: number;
+      };
     }
   }
 }
