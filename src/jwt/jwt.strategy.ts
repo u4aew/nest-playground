787659824386d981538b1d92985a5203aa3f44e0
Sergey Uchaev <dialoggg1994@gmail.com>
2024-01-28 18:52:04 +0300

    Add JWT auth

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -14,15 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    // В этом методе вы можете выполнить проверку данных, хранящихся в токене,
-    // и вернуть пользователя или выбросить исключение UnauthorizedException, если проверка не пройдет.
-    // Ваш JwtPayload может содержать, например, ID пользователя.
-    // Проверьте, что пользователь с указанным ID существует в вашей системе.
-    // Пример проверки:
-    // const user = await this.userService.findById(payload.userId);
-    // if (!user) {
-    //   throw new UnauthorizedException();
-    // }
-    // return user;
+    return { id: payload.sub };
   }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { JwtAuthGuard } from '../../jwt/jwt-auth.guard';
+import { Request } from 'express';
 
-@Controller('user')
-export class UserController {}
+@Controller('proxy/user')
+export class UserController {
+  // Здесь мы применяем JwtAuthGuard ко всем маршрутам в этом контроллере
+  @UseGuards(JwtAuthGuard)
+  @Get('info')
+  getUserInfo(@Req() req: Request) {
+    // Здесь мы извлекаем пользователя из запроса и возвращаем его.
+    // Предполагается, что req.user установлен JwtStrategy после успешной аутентификации.
+    const user = req.user;
+    return user;
+  }
+}
