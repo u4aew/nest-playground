f9816a6e37c979334b8af92a1750dd19ba06e74e
Sergey Uchaev <dialoggg1994@gmail.com>
2024-01-28 22:44:55 +0300

    Add reset password by email

--- src/modules/registration/registration.controller.ts
+++ src/modules/registration/registration.controller.ts
@@ -2,15 +2,18 @@ import { Controller, Post, Body, UseFilters } from '@nestjs/common';
 import { RegistrationService } from './registration.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ConfirmEmailDto } from './dto/confirm-email.dto';
-import { DatabaseExceptionFilter } from '../../shared/filters/database-exception.filter';
 import { RequestResetDto } from './dto/request-reset.dto';
 import { ConfirmResetDto } from './dto/confirm-reset.dto';
+import { DatabaseExceptionFilter } from '../../shared/filters/database-exception.filter';
 
 @Controller('proxy/registration')
 @UseFilters(new DatabaseExceptionFilter())
 export class RegistrationController {
   constructor(private readonly registrationService: RegistrationService) {}
 
+  /**
+   * Register a new user.
+   */
   @Post()
   async register(@Body() createUserDto: CreateUserDto) {
     return this.registrationService.register(
@@ -20,19 +23,26 @@ export class RegistrationController {
     );
   }
 
+  /**
+   * Confirm user email.
+   */
   @Post('confirm')
   async confirmEmail(@Body() confirmEmailDto: ConfirmEmailDto) {
     return this.registrationService.confirmEmail(confirmEmailDto.token);
   }
 
-  @Post('reset')
+  /**
+   * Request password reset.
+   */
+  @Post('reset/request')
   async requestReset(@Body() requestResetDto: RequestResetDto) {
-    console.log(requestResetDto, 'requestResetDto');
-
     return this.registrationService.requestPasswordReset(requestResetDto.email);
   }
 
-  @Post('reset-confirm')
+  /**
+   * Confirm password reset.
+   */
+  @Post('reset/confirm')
   async confirmReset(@Body() confirmResetDto: ConfirmResetDto) {
     return this.registrationService.resetPassword(
       confirmResetDto.token,
